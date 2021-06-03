c4ac6c8973143b19b84984e0470fcb0a39e92a77
qiang1234zhang <zhang1234qiang@sina.com>
2021-06-03 17:44:30 +0800

    🎉

--- src/views/member/test.vue
+++ src/views/member/test.vue
@@ -5,14 +5,53 @@
         <div class="box3">30px字体</div>
         <div class="box4">800px</div>
         <div class="box5">1000px</div>
-    </div>
+
+
+<div class="block">
+    <span class="demonstration">默认</span>
+    <el-date-picker style="width:400px"
+      v-model="date.value"
+      type="datetimerange"
+      range-separator="至"
+      start-placeholder="开始日期"
+      end-placeholder="结束日期">
+    </el-date-picker>
+  </div>
+        </div>
 </template>
 
 <script>
 export default {
-  name: "",
   data() {
-    return {};
+    return {
+        date:{value:[]}
+    }
+  },
+  created() {
+      this.getTime();
+  },
+  methods:{
+      getTime(){
+          const that=this;
+          const start= new Date();
+          const end=new Date();
+          start.setTime(start.getTime());
+          end.setTime(end.getTime()+3600*1000*24*6);
+          that.date={value:[that.formatDate(start),that.formatDate(end)]}
+          },
+        formatDate(date){
+            var year=date.getFullYear();
+            var month=date.getMonth()+1;
+            var day=date.getDate();
+            if(month<10){
+                month="0"+month;
+            }
+            if(day<10){
+                day="0"+day;
+            }
+            return year+"-"+month+"-"+day;
+        }
+
   }
 };
 </script>
